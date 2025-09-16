21ce959e4c9fb4abde8ee62d973cae4593594070
Eric Silberberg <72359394+esilberberg@users.noreply.github.com>
2025-09-16 21:22:30 +0000

    round 2 api issue

--- build_articles_oa_overview.py
+++ build_articles_oa_overview.py
@@ -10,6 +10,9 @@ import json
 import pandas as pd
 import streamlit as st
 import certifi
+import os
+
+os.environ['REQUESTS_CA_BUNDLE'] = certifi.where()
 
 def build_articles_oa_overview(orcid, jisc_api_key):
     """
@@ -101,17 +104,13 @@ def build_articles_oa_overview(orcid, jisc_api_key):
             return None
 
     def get_jisc_data(issn, jisc_api_key):
-        """
-        Retrieves journal permissions from the JISC Open Policy Finder.
-        """
         api_endpoint = f'https://v2.sherpa.ac.uk/cgi/retrieve_by_id?item-type=publication&api-key={jisc_api_key}&format=Json&identifier={issn}'
         try:
-            # Add the verify parameter to use certifi's certificate bundle
-            response = requests.get(api_endpoint, verify=certifi.where())
+            response = requests.get(api_endpoint)
             response.raise_for_status()
             return response.json()
         except requests.exceptions.RequestException as e:
-            print(f"Error making API request: {e}") 
+            print(f"Error making API request: {e}")
             return None
         except json.JSONDecodeError as e:
             print(f"Error decoding JSON response: {e}")
