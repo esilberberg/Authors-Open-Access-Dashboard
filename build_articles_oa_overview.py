adf04ac124c943f5fad8bc57d2c917fe311452f7
Eric Silberberg <72359394+esilberberg@users.noreply.github.com>
2025-09-16 21:25:52 +0000

    ended momentary test

--- build_articles_oa_overview.py
+++ build_articles_oa_overview.py
@@ -106,8 +106,7 @@ def build_articles_oa_overview(orcid, jisc_api_key):
     def get_jisc_data(issn, jisc_api_key):
         api_endpoint = f'https://v2.sherpa.ac.uk/cgi/retrieve_by_id?item-type=publication&api-key={jisc_api_key}&format=Json&identifier={issn}'
         try:
-            # Add the 'verify=False' parameter to skip SSL certificate verification
-            response = requests.get(api_endpoint, verify=False)
+            response = requests.get(api_endpoint)
             response.raise_for_status()
             return response.json()
         except requests.exceptions.RequestException as e:
